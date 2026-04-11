f64968bf5567c7e66b933f6bc74b3defa9984755
grisling <55872536+grisling@users.noreply.github.com>
2026-04-11 18:38:45 +0200

    tilføjet en masse kort funktionalitet som funktioner

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-//1-13 hjerter 14-26 kloer 27-39 ruder 40-52
+//1-13 hjerter 14-26 kloer 27-39 ruder 40-52 spar
 const STANDARDDECK = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52];
 let deck = [];
+let haand = [];
 resetDeck();
 console.log(deck);
 
@@ -11,11 +12,25 @@ function resetDeck(){
     
 }
 
-function faaKuloer(){
-
+function faaKuloer(kortnummer){
+    if (kortnummer <= 13)
+        return "Hjerter";
+    else if (kortnummer <= 26)
+        return "Kloer";
+    else if (kortnummer <= 39)
+        return "Ruder";
+    else if (kortnummer <= 52)
+        return "Spar";
 }
 
-function faaVaerdi(){
+function faaVaerdi(kortnummer){
+    if (kortnummer % 13 === 0){
+        return 13;
+    }
+    else{
+        return kortnummer % 13;
+    }
+    
 
 }
 //følgende funktion er lånt herfra: https://bost.ocks.org/mike/shuffle/
@@ -32,3 +47,18 @@ function bland(ublandetDeck){
     }
     return ublandetDeck;
 }
+
+function resethaand(){
+    haand = [];
+}
+
+function traekKort(){
+    haand.push(deck.pop());
+}
+
+function traekHaand(){
+    for (let i = 0; i < 5; i++){
+        traekKort();
+    }
+}
+
