67b9ed1ff15ab046bb05d6fd58a10c9f9f88d6f6
grisling <55872536+grisling@users.noreply.github.com>
2026-04-12 08:29:08 +0200

    har fixet bug med at man kan trække flere kort

--- script.js
+++ script.js
@@ -2,11 +2,7 @@
 const STANDARDDECK = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52];
 let deck = [];
 let haand = [];
-resetDeck();
-console.log(deck);
-traekHaand();
-console.log(haand);
-console.log(deck);
+
 
 function resetDeck(){
     deck = STANDARDDECK;
@@ -116,8 +112,10 @@ function nytSpil() {
 
 // Træk 5 kort og opdater visningen
 function trakHaandOgVis() {
+    if (deck.length < 5) {
     traekHaand();  
     opdaterVisning();
+}
 }
 
 // Når hele HTML-siden er indlæst (DOMContentLoaded), sætter vi knappernes funktioner og starter spillet
